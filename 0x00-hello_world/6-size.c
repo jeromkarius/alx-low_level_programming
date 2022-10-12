de346d368ce21be27acec473a1f41eaf4d425370
jeromkarius <joshriuki@gmail.com>
2022-10-12 06:32:39 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ printf("size of an int: %zu byte(s)\n", (unsigned long)sizeof(int));
 printf("size of a long int: %zu byte(s)\n", (unsigned long)sizeof(long int));
 printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("size of a float: %zu byte(s)\n", (unsigned long)sizeof(float));
-return (1);
+return (0);
 }
