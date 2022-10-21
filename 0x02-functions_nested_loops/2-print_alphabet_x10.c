bc04424940e03703f7c728845827f8f54c66d6e0
jeromkarius <joshriuki@gmail.com>
2022-10-21 03:13:12 +0000

    print_alphabet_x10.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+  * print_alphabet_x10 - print
+  * Description: print alphabet
+  * Return always 0 (success)
+  */
+
+void print_alphabet_x10(void)
+{
+char k;
+for (k = 'a'; k <= 'z'; k +=9)
+
+{
+_putchar (k);
+}
+
+return (0);
+}
