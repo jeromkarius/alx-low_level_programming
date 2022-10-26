d26b0fbe4df62f89b97fe32fb3cd9dbb302027ef
jeromkarius <joshriuki@gmail.com>
2022-10-26 10:59:10 +0000

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,27 +8,27 @@ void times_table(void)
 int a, b, c;
 for (a = 0; a <= 9; a++)
 {
-_putchar('0');
-_putchar(',');
-_putchar(' ');
 for (b = 0; b <= 9; b++)
 {
-c = (a * b);
-if ((c / 10) > 0)
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
 {
 _putchar('0');
-}
-else
+if (b != 0)
 {
 _putchar(' ');
-}
 _putchar((c % 10) + '0');
+}
 if (b < 9)
 {
 _putchar(',');
 _putchar(' ');
 }
 }
+}
 _putchar('\n');
+}
 }
 }
