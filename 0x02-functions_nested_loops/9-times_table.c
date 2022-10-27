ae85c1fe3e3c8508eaa06a1bfb5c8afb35b8b127
jeromkarius <joshriuki@gmail.com>
2022-10-27 12:45:39 +0000

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,21 @@ for (b = 0; b <= 9; b++)
 {
 c = a * b;
 if ((c / 10) > 0)
+{
 _putchar((c / 10) + '0');
+}
 else
+{
 _putchar(' ');
+}
 _putchar((c % 10) + '0');
 if (b < 9)
+{
 _putchar(',');
 _putchar(' ');
+}
+}
 _putchar('\n');
 }
 }
-}
+
