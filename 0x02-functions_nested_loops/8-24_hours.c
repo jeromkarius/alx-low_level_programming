ddb0793afcd2c62a890f265eae956c4475cd4143
jeromkarius <joshriuki@gmail.com>
2022-10-27 10:57:24 +0000

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ for (m = 0; m < 60; m++)
 _putchar((h / 10) + '0');
 _putchar((h % 10) + ':');
 _putchar((m / 10) + '0');
-_putchar((m / 10) + ':');
+_putchar((m / 10) + '0');
 _putchar('\n');
 }
 }
