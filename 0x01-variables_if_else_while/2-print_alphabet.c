1e62d9b8d01290cc2ca7c1016e20ec95179a4638
jeromkarius <joshriuki@gmail.com>
2022-10-29 18:28:30 +0000

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,10 @@
 int main(void)
 {
 char ch;
-for (ch = 'a'; ch <= 'm'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
-for (ch = 'n'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
+putchar('\n');
 return (0);
 }
