a8b5756405b433d0eb9e6bf0cde3c193dc079595
jeromkarius <joshriuki@gmail.com>
2022-10-30 04:12:47 +0000

    soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 /**
-  *main - entry point
-  *return 0
+  * main - prints some letters of alphabet
+  *
+  * Return: Always success
   */
 int main(void)
 {
 char ch;
-for (ch = 'a'; ch <= 'd'; ch++)
-for (ch = 'f'; ch <= 'p'; ch++)
-for (ch = 'r'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
 {
 putchar(ch);
+}
 }
 putchar('\n');
 return (0);
