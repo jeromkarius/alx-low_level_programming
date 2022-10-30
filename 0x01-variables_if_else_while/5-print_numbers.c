a45d11130096390537c63e15aa45a642c7d720d6
jeromkarius <joshriuki@gmail.com>
2022-10-30 04:50:04 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+  * main - prints digit to base 10
+  *
+  * Return: always (success)
+  */
+
+int main(void)
+{
+int a = 0;
+while (a <= 9)
+{
+printf("%d\n", a);
+a++;
+}
+return (0);
+}
